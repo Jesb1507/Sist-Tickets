dc9a61d584f3e083787f54e2a1ffbd01d6ab73d9
JF-2000 <65424365+JF-2000@users.noreply.github.com>
2020-08-07 17:26:02 -0400

    compra

--- consulta.php
+++ consulta.php
@@ -13,16 +13,4 @@
         return $result=$mysqli->query($sql); 
     }
 
-	// function lista_fecha(){		
-	// 	include('database.php');	
-	// 	$sql="SELECT * FROM rutas";
-	// 	return $result=$mysqli->query($sql); 
-	// }
-
-	// function lista_hora(){		
-	// 	include('database.php');	
-	// 	$sql="SELECT * FROM rutas";
-	// 	return $result=$mysqli->query($sql); 
-	// }
-
 ?>
